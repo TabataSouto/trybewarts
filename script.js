87f8b8f1188fe6927957bcbdf79ebe96dd25ffc3
Brenndha <brenndha.cabral@yahoo.com>
2021-12-02 16:37:30 -0300

    requisito 3: correções

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-
 const inputEmail = document.querySelector('#email');
 const emailTeste = 'tryber@teste.com';
 const senhaTeste = '123456';
 const inputSenha = document.querySelector('#senha');
 const botaoSubmit = document.querySelector('#botao');
 
-
 function validacaoBotao(event) {
   event.preventDefault();
-  if (inputSenha.value === senhaTeste  && inputEmail.value === emailTeste) {
+  if (inputSenha.value === senhaTeste && inputEmail.value === emailTeste) {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
